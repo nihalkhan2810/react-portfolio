b3712f9d26066fbf1005373788e935179b4418f2
nihalkhan2810 <mohammednihal281001@gmail.com>
2025-11-21 10:06:10 -0500

    Changed name from Resume to ResumeSection

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
-import Resume from './components/Resume';
+import Resume from './components/ResumeSection';
 // Import the ChatbotComponent
 import ChatbotComponent from './components/ChatbotComponent'; // Adjust path if needed
 
